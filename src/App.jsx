f1e2e03d4dca32562d86f5d373495f1dbdb084de
vanderleik <vanderleik@yahoo.com.br>
2024-02-09 10:23:47 -0300

    feat: criando componente ThankYou

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
 import './App.css'
 import Catalog from './components/Catalog'
 import Cart from './components/Cart'
+import ThankYou from './components/ThankYou'
 
 function App() { 
 
@@ -9,13 +10,14 @@ function App() {
     <BrowserRouter>
       <nav>
         <Link to="/">Catálogo</Link>
-        <Link to="/cart">Carrinho</Link>
+        <Link to="/cart">Carrinho</Link>        
       </nav>
 
       <div className="container">
         <Routes>
           <Route path="/" element={<Catalog />} />
           <Route path="/cart" element={<Cart />} />
+          <Route path="/thank-you" element={<ThankYou />} />
         </Routes>
       </div>
     </BrowserRouter>
