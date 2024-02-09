ceb1766c49ed9994a8f3e199ae1a0e31f7f73505
vanderleik <vanderleik@yahoo.com.br>
2024-02-09 10:35:05 -0300

    feat: adicionando as informações no component Product

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
 
-const Product = () => {
+const Product = ({product}) => {
   return (
-    <div>Product</div>
+    <div className='product'>
+        <img src={product.image} alt={product.name} />
+        <h3>{product.title}</h3>
+        <p>${product.price}</p>
+        <div className="cart-buttons">
+            <select>
+                <option value="1">1</option>
+            </select>
+            <button>Adicionar ao carrinho</button>
+        </div>
+    </div>
   )
 }
 
